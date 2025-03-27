76b2b8efe03f46af9c86627023e6065ddbebd2e1
luansilva25 <luansilva250807@gmail.com>
2025-03-26 21:51:09 -0300

    FEAT: category model created [refs:#4]

--- src/core/gohost/models/hotel.py
+++ src/core/gohost/models/hotel.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.uploader.models import Image
 from core.usuario.models import Usuario as User
+from .category import Category
 
 class Hotel(models.Model):
     covers = models.ManyToManyField(Image, related_name='hotel')
@@ -10,6 +11,7 @@ class Hotel(models.Model):
     admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None, related_name='hotel')
     rate = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
     description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=None, null=True, blank=True)
     
     def __str__(self):
         return self.title
