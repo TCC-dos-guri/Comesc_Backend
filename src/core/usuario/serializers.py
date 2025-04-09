d684d89b78511ce5c834a2ee2e7be0f9066e110f
luansilva25 <luansilva250807@gmail.com>
2025-04-09 20:10:21 -0300

    FEAT: create worker serializer [refs:#49]

--- src/core/usuario/serializers.py
+++ src/core/usuario/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
-from .models import Usuario
+from .models import Usuario, Worker
 
 
 class UsuarioSerializer(ModelSerializer):
     class Meta:
         model = Usuario
         fields = "__all__"
+
+class WorkerSerializer(ModelSerializer):
+    class Meta:
+        model = Worker
+        fields = '__all__'
+
