37cf3b6de01556667fbe93ae85b78407e7616b5d
renascm_ <renatochagas.m@gmail.com>
2025-04-10 20:35:25 -0300

    FEAT: create batch serialiazer

--- src/core/comesc/serializer/__init__.py
+++ src/core/comesc/serializer/__init__.py
@@ -4,3 +4,5 @@ from .state import StateSerializer
 from .supplier import SupplierSerializer, SupplierCreateSerializer
 from .roll import RollSerializer
 from .material import MaterialSerializer, MaterialCreateSerializer
+from .batch import BatchSerializer, BatchCreateSerializer
+from .inventory import InventorySerializer, InventoryCreateSerializer
