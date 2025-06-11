f3775e90092d0ef66d7095ed0d999bb73ebc23d2
luansilva25 <luansilva250807@gmail.com>
2025-06-11 14:06:23 -0300

    FEAT: config deploy #91

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p+gu%4j6yqo$k99i+*3s(dudfh$pzqk(ha=#dpoinv3r2xy&&m'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 'True')
+MODE = os.environ.get("MODE", "dev")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,12 +89,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+
+
+# DATABASES = {
+#        'default': dj_database_url.config(
+#            default='sqlite:///db.sqlite3',
+#            conn_max_age=600,
+#            conn_health_checks=True,
+#        )
+# }
+
+DATABASES = {'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True), }
+
 
 
 # Password validation
