31d5f1d349f2d2b9d84442b7b9ba450ae925512c
luansilva25 <luansilva250807@gmail.com>
2025-10-29 09:13:35 -0300

    REFACTOR: removing genai

--- src/core/comesc/views/report.py
+++ src/core/comesc/views/report.py
@@ -1,15 +1,12 @@
-from weasyprint import HTML
-from django.template.loader import render_to_string
-from django.http import HttpResponse
-from django.views import View
 from core.comesc.models.batch import Batch
 from core.comesc.models.roll import Roll
 from core.comesc.serializer.roll import RollSerializer
 from core.comesc.serializer.batch import BatchSerializer
-from config.gemini_config import generate_report
-import json 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-class ReportView(View):
+class ReportView(APIView):
     def get(self, request, id):
         batch = Batch.objects.get(id=id)
         batch_rolls = Roll.objects.filter(batch=batch)
@@ -21,16 +18,6 @@ class ReportView(View):
             'rolls': rolls_data
         }
 
-        dados_como_string = json.dumps(final_data)
 
-        print("\nIniciando a geração do relatório a partir do arquivo principal...")
-
-        relatorio_html = generate_report(dados_como_string)
-
-        pdf_file = HTML(string=relatorio_html).write_pdf()
-
-        response = HttpResponse(pdf_file, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-
-        return response
+        return Response(final_data, status=status.HTTP_200_OK)
     
