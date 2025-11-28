a73d40bc8a60ab3163df9095a766ebaf9a0dc36e
luansilva25 <luansilva250807@gmail.com>
2025-11-28 08:13:29 -0300

    FEAT: mudei umas coisas não lembro

--- src/core/comesc/models/roll.py
+++ src/core/comesc/models/roll.py
@@ -5,9 +5,9 @@ from .batch import Batch
 
 class Roll(models.Model):
     production_order = models.AutoField(primary_key=True, unique=True)
-    color = models.ForeignKey(Color, on_delete=models.PROTECT, related_name='+', null=True, blank=True)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
     kg = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-    batch = models.ForeignKey(Batch, on_delete=models.PROTECT, default=None, related_name='roll')
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, default=None, related_name='roll')
     nonconformity = models.BooleanField(default=False)
 
     def __str__(self):
